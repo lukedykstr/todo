5abf95e65643fa46604882cf61741016934ac9e7
Luke Dykstra <dykstrlu@mail.gvsu.edu>
2024-01-29 14:45:33 -0500

    added descriptive comments for todo.c

--- todo.c
+++ todo.c
@@ -1,16 +1,36 @@
+/*
+ * todo.c
+ *
+ * A basic command line program that creates and manages tasks
+ * with information such as notes and due dates. All task data
+ * is stored in todo.txt.
+ *
+ * Written by Luke Dykstra 1/29/24
+ *
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include "vector.h"
 
+// Struct that holds information about the command and arguments
+// provided when the program is run from the command line
 typedef struct cmdinfo {
-	char* cmd;
-	char* arg;
-	char* opt_n;
-	char* opt_d;
+	char* cmd;   // arg 1
+	char* arg;   // arg 2, if not an option
+	char* opt_n; // -n [string]
+	char* opt_d; // -d [string
 } cmdinfo;
 
+/* 
+ * Given argc and argv from main, create a struct that holds
+ * parsed information about the arguments from the command line
+ *
+ * Returns pointer to a cmdinfo struct that holds the information,
+ * or NULL if no command was provided
+ */
 cmdinfo* get_cmd_info(int argc, char* argv[]) {
 	if (argc > 1) {
 		// Create new cmdinfo struct to store info
@@ -31,10 +51,11 @@ cmdinfo* get_cmd_info(int argc, char* argv[]) {
 			}
 			// Check all arguments provided to see if they are a valid option
 			for(int i = index; i < argc - 1; i ++) {
-				// -n [note_string]
 				if(!strcmp(argv[i], "-n")) {
+					// -n [note]
 					cmd -> opt_n = argv[++ i];
 				} else if (!strcmp(argv[i], "-d")) {
+					// -d [date]
 					cmd -> opt_d = argv[++ i];
 				}
 			}
@@ -46,6 +67,12 @@ cmdinfo* get_cmd_info(int argc, char* argv[]) {
 	}
 }
 
+/*
+ * Creates a task given the message / task name, note, and due date.
+ * Set note or due date to NULL if none provided.
+ *
+ * Returns nothing
+ */
 void todo_add(char* msg, char* note, char* due) {
 	FILE* file = fopen("todo.txt", "a+");
 
@@ -53,10 +80,16 @@ void todo_add(char* msg, char* note, char* due) {
 		printf("todo: failure to create or open todo file\n");
 		exit(1);
 	}
-
+	// Get the current time as a string
 	time_t curr_time = time(NULL);
 	char* timestr = ctime(&curr_time);
 
+	// Task format:
+	// name
+	// time created
+	// (note) OR \n if none
+	// (due date) OR \n if none
+	// end
 	fputs(msg, file);
 	fputs("\n", file);
 	fputs(timestr, file);
@@ -79,15 +112,23 @@ void todo_add(char* msg, char* note, char* due) {
 }
 
 int main(int argc, char* argv[]) {
+	// Get command info given argc and argv
 	cmdinfo* cmd = get_cmd_info(argc, argv);
 
 	if (cmd != NULL) {
 		if (!strcmp(cmd -> cmd, "all")) {
 
 		} else {
+			// If first argument provided does not match with a valid command,
+			// create task with arg1 (cmd -> cmd) as the name
+			//
+			// ex. todo "physics homework"
 			todo_add(cmd -> cmd, cmd -> opt_n, cmd -> opt_d);
 		}
+		// cmd was manually allocated, so we must free it
+		free(cmd);
 	} else {
+		// No command provided
 		printf("todo: no action supplied\n");
 	}
 }
